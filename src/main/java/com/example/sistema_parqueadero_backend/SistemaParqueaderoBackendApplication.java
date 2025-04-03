7e87124e1c7430bbacfbce1445b916789b95ae69
Oweenncito <owenfernando08@gmail.com>
2025-04-03 18:09:25 -0500

    commit de bienvenida

---
+++ src/main/java/com/example/sistema_parqueadero_backend/SistemaParqueaderoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_parqueadero_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaParqueaderoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaParqueaderoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_parqueadero_backend/SistemaParqueaderoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_parqueadero_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaParqueaderoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
