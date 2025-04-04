78649dd2e8c5265b76cac23df94a85fb6ae3f660
Oweenncito <owenfernando08@gmail.com>
2025-04-04 11:29:39 -0500

    cambios a la clase mensualidad

--- src/main/java/com/example/sistema_parqueadero_backend/ClienteMensualidad.java
+++ src/main/java/com/example/sistema_parqueadero_backend/ClienteMensualidad.java
@@ -5,8 +5,7 @@ public class ClienteMensualidad {
     public String nombre;
     public String apellido;
     public String id;
-    public String celular;
-    public String marca;
+  
     public String placaVehiculo;
     
     public ClienteMensualidad() {
@@ -14,12 +13,11 @@ public class ClienteMensualidad {
     	this.id = UUID.randomUUID().toString();
     }
     
-    public ClienteMensualidad(String nombre, String apellido, String celular, String marca, String placaVehiculo){
+    public ClienteMensualidad(String nombre, String apellido, String placaVehiculo){
         this.nombre = nombre;
         this.apellido = apellido;
         this.id = UUID.randomUUID().toString();
-        this.celular = celular;
-        this.marca = marca;
+     
         this.placaVehiculo = placaVehiculo;
     }
     
@@ -43,16 +41,8 @@ public class ClienteMensualidad {
         return id;
     }
     
-    public void setCedula(String id){
+    public void setId(String id){
         this.id = id;
-    }
-    
-    public String getCelular(){
-        return celular;
-    }
-    
-    public void setCelular(String celular){
-        this.celular = celular;
     }
     
     public String getPlacaVehiculo(){
